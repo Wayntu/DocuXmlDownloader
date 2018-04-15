939a7743d4f5168dacfcba1158ff5e376f62808f
Waynet <kartd0094775@gmail.com>
2018-04-15 22:06:20 +0800

    Apr 15, 2018 #put the db, corpus info into xml

--- js/DocuSkyExport.js
+++ js/DocuSkyExport.js
@@ -1,80 +1,134 @@
-let UserDefinedTag = ''
-const parseUserDefinedTag = (content) => {
-    const tagName = {}
-    const nodeList = content.firstChild.firstChild.childNodes
-    for (let i = 0; i　< nodeList.length; i++) {
-        let node = nodeList[i]
-        if (node.nodeName.substr(0,4) == 'Udef') {    // nodename: Udef_XXX
-            tagName[node.nodeName] = node.nodeName
-        }
-    }
-    for (let key in tagName) {
-        UserDefinedTag += "<tag default_sub_category='-' default_category='" + key + "' type='contentTagging'>" + key + "</tag>" 
-    }
-}
-const convertContent = (docContentXml) => {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(docContentXml, "text/xml");
-      parseUserDefinedTag(xmlDoc)
-      const nodeList = xmlDoc.firstChild.childNodes
-      let xmlString = ''
-      for (let i = 0; i < nodeList.length; i++) {
-            if (nodeList[i].nodeName === 'Paragraph')
-                  xmlString += nodeList[i].outerHTML
+class DocuSkyExporter {
+      constructor() {
+            this.corpus = ''
+            this.db = ''
+            this.UserDefinedTag = ''
+            this.featureAnalysis = {}
+
+            this.parseUserDefinedTag = this.parseUserDefinedTag.bind(this)
       }
-      return xmlString
-}
-const convertMetadata = (docMetadataXml) => {
-      if (docMetadataXml === undefined) return ''
-      const parser = new DOMParser()
-      const xmlDoc = parser.parseFromString(docMetadataXml, "text/xml")
-      let xmlString = ''
-      if (xmlDoc.firstChild.nodeName === "DocMetadata")
-            xmlString = xmlDoc.firstChild.innerHTML
-      return xmlString
-}
-const convertTitle = (docTitleXml) => {
-      if (docTitleXml === undefined) return ''
-      const parser = new DOMParser()
-      const xmlDoc = parser.parseFromString(docTitleXml, "text/xml")
-      let xmlString = ''
-      if (xmlDoc.firstChild.nodeName === 'DocTitle')
-            xmlString = xmlDoc.firstChild.innerHTML
-      return xmlString
-}
-const convertTimeInfo = ({
-      dateOrigStr='',
-      dateDynasty='',
-      dateEra='',
-      dateChNormYear='',
-      dateAdDate='',
-      dateAdYear='',
-      timeseqType='',
-      timeseqNumber='',
-      }) => {
-      return "<time_orig_str>" + dateOrigStr + "</time_orig_str>"
-            + "<time_dynasty>" + dateDynasty + "</time_dynasty>"
-            + "<time_era>" + dateEra + "</time_era>"
-            + "<time_norm_year>" + dateChNormYear + "</time_norm_year>"
-            + "<time_ad_date>" + dateAdDate + "</time_ad_date>"
-            + "<time_ad_year>" + dateAdYear + "</time_ad_year>"
-            + "<timeseq_type>" + timeseqType + "</timeseq_type>"
-            + "<timeseq_number>" + timeseqNumber + "</timeseq_number>"
-}
-const convertPlaceInfo = ({
-      geoLevel1='',
-      geoLevel2='',
-      geoLevel3='',
-      geoX='',
-      geoY='',
-      }) => {
-      return "<geo_level1>" + geoLevel1 + "</geo_level1>"
-            + "<geo_level2>" + geoLevel2 + "</geo_level2>"
-            + "<geo_level3>" + geoLevel3 + "</geo_level3>"
-            + "<geo_longitude>" + geoX + "</geo_longitude>"
-            + "<geo_latitude>" + geoY + "</geo_latitude>"
-}
-const parseDocument = ({
+      generateDocuXml({documents, db, corpus}) {
+            let documentXml = ''
+            this.db = db
+            this.corpus = corpus
+            for (let number in documents) {
+                  let document = documents[number]
+                  documentXml += this.parseDocument({...document, number})
+            }
+            const xmlString = "<ThdlPrototypeExport>"
+                              + "<corpus name='*'>"
+                              + this.generateFeatureAnalysisInfo()
+                              + "</corpus>"
+                              + "<documents>"
+                              + documentXml
+                              + "</documents></ThdlPrototypeExport>"
+            return xmlString
+      }
+      generateFeatureAnalysisInfo() {
+            let result = '<feature_analysis>'
+            for (let key in this.featureAnalysis) {
+                  result += "<tag name='" + key + "' default_sub_category='-' default_category='" + key + "' type='contentTagging'/>"
+            }
+            result += '</feature_analysis>'
+            return result
+      }
+      parseUserDefinedTag(content) {
+            this.UserDefinedTag = ''
+            const tagName = {}
+            const nodeList = content.firstChild.firstChild.childNodes
+            for (let i = 0; i　< nodeList.length; i++) {
+                  let node = nodeList[i]
+                  if (node.nodeName.substr(0,4) == 'Udef') {    // nodename: Udef_XXX
+                        tagName[node.nodeName] = node.nodeName
+                        if (!(node.nodeName in this.featureAnalysis)) this.featureAnalysis[node.nodeName] = node.nodeName
+                  }
+            }
+            for (let key in tagName) {
+                  this.UserDefinedTag += "<tag default_sub_category='-' default_category='" + key + "' type='contentTagging'>" + key + "</tag>"
+            }
+      }
+      convertContent(docContentXml) {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(docContentXml, "text/xml");
+            this.parseUserDefinedTag(xmlDoc)
+            const nodeList = xmlDoc.firstChild.childNodes
+            let xmlString = ''
+            for (let i = 0; i < nodeList.length; i++) {
+                  if (nodeList[i].nodeName === 'Paragraph')
+                        xmlString += nodeList[i].outerHTML
+            }
+            return xmlString
+      }
+      convertMetadata(docMetadataXml) {
+            if (docMetadataXml === undefined) return ''
+            const parser = new DOMParser()
+            const xmlDoc = parser.parseFromString(docMetadataXml, "text/xml")
+            let xmlString = ''
+            if (xmlDoc.firstChild.nodeName === "DocMetadata")
+                  xmlString = xmlDoc.firstChild.innerHTML
+            return xmlString
+      }
+      convertTitle(docTitleXml) {
+            if (docTitleXml === undefined) return ''
+            const parser = new DOMParser()
+            const xmlDoc = parser.parseFromString(docTitleXml, "text/xml")
+            let xmlString = ''
+            if (xmlDoc.firstChild.nodeName === 'DocTitle')
+                  xmlString = xmlDoc.firstChild.innerHTML
+            return xmlString
+      }
+      convertTimeInfo({
+            dateOrigStr='',
+            dateDynasty='',
+            dateEra='',
+            dateChNormYear='',
+            dateAdDate='',
+            dateAdYear='',
+            timeseqType='',
+            timeseqNumber='',
+            }) {
+            return "<time_orig_str>" + dateOrigStr + "</time_orig_str>"
+                  + "<time_dynasty>" + dateDynasty + "</time_dynasty>"
+                  + "<time_era>" + dateEra + "</time_era>"
+                  + "<time_norm_year>" + dateChNormYear + "</time_norm_year>"
+                  + "<time_ad_date>" + dateAdDate + "</time_ad_date>"
+                  + "<time_ad_year>" + dateAdYear + "</time_ad_year>"
+                  + "<timeseq_type>" + timeseqType + "</timeseq_type>"
+                  + "<timeseq_number>" + timeseqNumber + "</timeseq_number>"
+      }
+      convertTimeInfo({
+            dateOrigStr='',
+            dateDynasty='',
+            dateEra='',
+            dateChNormYear='',
+            dateAdDate='',
+            dateAdYear='',
+            timeseqType='',
+            timeseqNumber='',
+            }) {
+            return "<time_orig_str>" + dateOrigStr + "</time_orig_str>"
+                  + "<time_dynasty>" + dateDynasty + "</time_dynasty>"
+                  + "<time_era>" + dateEra + "</time_era>"
+                  + "<time_norm_year>" + dateChNormYear + "</time_norm_year>"
+                  + "<time_ad_date>" + dateAdDate + "</time_ad_date>"
+                  + "<time_ad_year>" + dateAdYear + "</time_ad_year>"
+                  + "<timeseq_type>" + timeseqType + "</timeseq_type>"
+                  + "<timeseq_number>" + timeseqNumber + "</timeseq_number>"
+      }
+      convertPlaceInfo({
+            geoLevel1='',
+            geoLevel2='',
+            geoLevel3='',
+            geoX='',
+            geoY='',
+            }) {
+            return "<geo_level1>" + geoLevel1 + "</geo_level1>"
+                  + "<geo_level2>" + geoLevel2 + "</geo_level2>"
+                  + "<geo_level3>" + geoLevel3 + "</geo_level3>"
+                  + "<geo_longitude>" + geoX + "</geo_longitude>"
+                  + "<geo_latitude>" + geoY + "</geo_latitude>"
+      }
+      parseDocument({
             // 必要資訊
             number,
             corpus,                 // corpus
@@ -101,43 +155,29 @@ const parseDocument = ({
             docTimeCreated='',
             xmlFormatName='',
             // extraMetadata='',    // DocuSky不支援?
-      }) => {
+      }) {
             const parser = new DOMParser()
             let xmlString = "<document filename='" + docFilename + "' number='" + number + "'>"
-                  + "<corpus corpus_order='" + corpusOrder + "'>" + corpus + "</corpus>"
+                  + "<corpus corpus_order='" + corpusOrder + "'>" + this.corpus + "</corpus>" // 這邊的 corpus 與 this.corpus 應該相同
                   + "<compilation>" + docCompilation + "</compilation>"
-                  + "<doc_content>" + convertContent(docContentXml) + "</doc_content>"
-                  + "<xml_metadata>" + convertMetadata(docMetadataXml) + "</xml_metadata>"
-                  + "<title>" + convertTitle(docTitleXml) + "</title>"
+                  + "<doc_content>" + this.convertContent(docContentXml) + "</doc_content>"
+                  + "<xml_metadata>" + this.convertMetadata(docMetadataXml) + "</xml_metadata>"
+                  + "<title>" + this.convertTitle(docTitleXml) + "</title>"
                   + "<doc_source doc_source_order='" + docSourceOrder + "'>" + docSource + "</doc_source>"
                   + "<doctype>" + docType + "</doctype>"
                   + "<docclass>" + docXmlFormatSubname + "</docclass>"
-                  + convertTimeInfo(timeInfo)
-                  + convertPlaceInfo(placeInfo)
+                  + this.convertTimeInfo(timeInfo)
+                  + this.convertPlaceInfo(placeInfo)
                   + "<author>" + author + "</author>"
-                  + "<doc_user_tagging>" + UserDefinedTag + "</doc_user_tagging>"
+                  + "<doc_user_tagging>" + this.UserDefinedTag + "</doc_user_tagging>"
                   + "<topic topic_order='" + docTopicL1Order + "'>" + docTopicL1 + "</topic>"
                   + "<doc_id>" + docId + "</doc_id>"
                   + "<doc_time_created>" + docTimeCreated + "</doc_time_created>"
                   + "<xml_format_name>" + xmlFormatName + "</xml_format_name>"
+                  + "<db>" + this.db + "</db>"
                   // + "<extra_metadata>" + extraMetadata + "</extra_metadata>"
                   + "</document>"
             const xmlDoc = parser.parseFromString(xmlString, "text/xml")
             return xmlString
-}
-
-const exportDocuXml = ({documents, db, corpus}) => {
-    let documentXml = ''
-    for (let number in documents) {
-        let document = documents[number]
-        documentXml += parseDocument({...document, number})
-    }
-    const xmlString = "<ThdlPrototypeExport>"
-                    + "<db>" + db + "</db>"
-                    + "<corpus name='" + corpus + "'>"
-                    + "</corpus>"
-                    + "<documents>"
-                    + documentXml
-                    + "</documents></ThdlPrototypeExport>"
-    return xmlString
+      }
 }
--- js/DocuSkyImport.js
+++ js/DocuSkyImport.js
@@ -1,126 +1,133 @@
-const importDocuXml = (xmlDoc) => {
-    const root = xmlDoc.firstChild.childNodes   // <ThdlPrototype>
-    const db = root[0]
-    const corpus = root[1]
-    const documents = root[2].childNodes
-    const docList = []
-    documents.forEach((document) => {
-        docList.push(transform(document))
-    })
-    return {db, corpus, docList}
-}
-const wrapUserDefinedTag = (xmlString) => {
-    const parser = new DOMParser()
-    const xmlDoc = parser.parseFromString("<div>" + xmlString + "</div>", "text/xml")
-    const result = []
-    xmlDoc.firstChild.childNodes.forEach((tag) => {
-        result.push(tag.innerHTML)
-    })
-    return result
-}
-const transform = (document) => {
-    const filename = document.attributes[0].value
-    const number = document.attributes[1].value
-    const docInfo = {}
-    const timeInfo = {}
-    const placeInfo = {}
-    document.childNodes.forEach((childNode) => {
-        // if (childNode.innerHTML === '') continue
-        switch (childNode.nodeName) {
-            case "corpus":
-                docInfo['corpus'] = childNode.innerHTML
-                docInfo['corpusOrder'] = childNode.attributes[0].value
-                break;
-            case "compilation":
-                docInfo['docCompilation'] = childNode.innerHTML
-                break;
-            case "doc_content":
-                docInfo['docContentXml'] = "<Content>" + childNode.innerHTML + "</Content>"
-                break;
-            case "xml_metadata":
-                docInfo['docMetadataXml'] = "<DocMetadata>" + childNode.innerHTML + "</DocMetadata>"
-                break;
-            case "title":
-                docInfo['docTitleXml'] = "<DocTitle>" + childNode.innerHTML + "</DocTitle>"
-                break;
-            case "doc_source":
-                docInfo['docSource'] = childNode.innerHTML
-                docInfo['docSourceOrder'] = childNode.attributes[0].value
-                break;    
-            case "doctype":
-                docInfo['docType'] = childNode.innerHTML
-                break;
-            case "docclass":
-                docInfo['docXmlFormatSubname'] = childNode.innerHTML
-                break;
-            case "time_orig_str":
-                timeInfo['dateOrigStr'] = childNode.innerHTML
-                break;
-            case "time_dynasty":
-                timeInfo['dateDynasty'] = childNode.innerHTML
-                break;
-            case "time_era":
-                timeInfo['dateEra'] = childNode.innerHTML
-                break;
-            case "time_norm_year":
-                timeInfo['dateChNormYear'] = childNode.innerHTML
-                break;
-            case "time_ad_date":
-                timeInfo['dateAdDate'] = childNode.innerHTML
-                break;
-            case "time_ad_year":
-                timeInfo['dateAdYear'] = childNode.innerHTML
-                break;
-            case "timeseq_type":
-                timeInfo['timeseqType'] = childNode.innerHTML
-                break;
-            case "timeseq_number":
-                timeInfo['timeseqNumber'] = childNode.innerHTML
-                break;
-            case "geo_level1":
-                placeInfo['geoLevel1'] = childNode.innerHTML
-                break;
-            case "geo_level2":
-                placeInfo['geoLevel2'] = childNode.innerHTML
-                break;
-            case "geo_level3":
-                placeInfo['geoLevel3'] = childNode.innerHTML
-                break;
-            case "geo_longitude":
-                placeInfo['geoX'] = childNode.innerHTML
-                break;
-            case "geo_latitude":
-                placeInfo['geoY'] = childNode.innerHTML
-                break;
-            case "author":
-                docInfo['docAuthor'] = childNode.innerHTML
-                break;
-            case "doc_user_tagging":
-                docInfo['docUserTagging'] = wrapUserDefinedTag(childNode.innerHTML)
-                break;
-            case "topic":
-                docInfo['docTopicL1'] = childNode.innerHTML
-                docInfo['docTopicL1Order'] = childNode.attributes[0].value
-                break;
-            case "doc_id":
-                docInfo['docId'] = childNode.innerHTML
-                break;
-            case "doc_time_created":
-                docInfo['doc_time_created'] = childNode.innerHTML
-                break;
-            case "xml_format_name":
-                docInfo['xmlFormatName'] = childNode.innerHTML
-                break;
-            // case "extra_metadata":
-            //     docInfo['extraMetadata'] = childNode.innerHTML
-            //     break;
-            default:
-                console.log('Unknow information: ' + childNode.nodeName)
-                break;
-        }
-    })
-    docInfo['docFilename'] = filename
-    docInfo['timeInfo'] = timeInfo
-    docInfo['placeInfo'] = placeInfo 
-    return { number , docInfo }
+class DocuSkyImporter {
+    generateDocInfo(xmlDoc) {
+        const root = xmlDoc.firstChild.childNodes   // <ThdlPrototype>
+        console.log(root)
+        const documents = root[1].childNodes
+        const docList = []
+
+        documents.forEach((document) => {
+            docList.push(this.transform(document))
+        })
+        return docList
+    }
+    wrapUserDefinedTag(xmlString) {
+        const parser = new DOMParser()
+        const xmlDoc = parser.parseFromString("<div>" + xmlString + "</div>", "text/xml")
+        const result = []
+        xmlDoc.firstChild.childNodes.forEach((tag) => {
+            result.push(tag.innerHTML)
+        })
+        return result
+    }
+    transform(document) {
+        const filename = document.attributes['filename'].value
+        const number = (document.attributes['number'])? document.attributes['number'].value : 0
+        const docInfo = {}
+        const timeInfo = {}
+        const placeInfo = {}
+        document.childNodes.forEach((childNode) => {
+            // if (childNode.innerHTML === '') continue
+            switch (childNode.nodeName) {
+                case "corpus":
+                    docInfo['corpus'] = childNode.innerHTML
+                    docInfo['corpusOrder'] = (childNode.attributes['corpus_order'])? childNode.attributes['corpus_order'].value : "0"
+                    break;
+                case "compilation":
+                    docInfo['docCompilation'] = childNode.innerHTML
+                    break;
+                case "doc_content":
+                    docInfo['docContentXml'] = "<Content>" + childNode.innerHTML + "</Content>"
+                    break;
+                case "xml_metadata":
+                    docInfo['docMetadataXml'] = "<DocMetadata>" + childNode.innerHTML + "</DocMetadata>"
+                    break;
+                case "title":
+                    docInfo['docTitleXml'] = "<DocTitle>" + childNode.innerHTML + "</DocTitle>"
+                    break;
+                case "doc_source":
+                    docInfo['docSource'] = childNode.innerHTML
+                    docInfo['docSourceOrder'] = (childNode.attributes['doc_source_order'])? childNode.attributes['doc_source_order'].value : "0"
+                    break;    
+                case "doctype":
+                    docInfo['docType'] = childNode.innerHTML
+                    break;
+                case "docclass":
+                    docInfo['docXmlFormatSubname'] = childNode.innerHTML
+                    break;
+                case "time_orig_str":
+                    timeInfo['dateOrigStr'] = childNode.innerHTML
+                    break;
+                case "time_dynasty":
+                    timeInfo['dateDynasty'] = childNode.innerHTML
+                    break;
+                case "time_era":
+                    timeInfo['dateEra'] = childNode.innerHTML
+                    break;
+                case "time_norm_year":
+                    timeInfo['dateChNormYear'] = childNode.innerHTML
+                    break;
+                case "time_ad_date":
+                    timeInfo['dateAdDate'] = childNode.innerHTML
+                    break;
+                case "time_ad_year":
+                    timeInfo['dateAdYear'] = childNode.innerHTML
+                    break;
+                case "timeseq_type":
+                    timeInfo['timeseqType'] = childNode.innerHTML
+                    break;
+                case "timeseq_number":
+                    timeInfo['timeseqNumber'] = childNode.innerHTML
+                    break;
+                case "geo_level1":
+                    placeInfo['geoLevel1'] = childNode.innerHTML
+                    break;
+                case "geo_level2":
+                    placeInfo['geoLevel2'] = childNode.innerHTML
+                    break;
+                case "geo_level3":
+                    placeInfo['geoLevel3'] = childNode.innerHTML
+                    break;
+                case "geo_longitude":
+                    placeInfo['geoX'] = childNode.innerHTML
+                    break;
+                case "geo_latitude":
+                    placeInfo['geoY'] = childNode.innerHTML
+                    break;
+                case "author":
+                    docInfo['docAuthor'] = childNode.innerHTML
+                    break;
+                case "doc_user_tagging":
+                    docInfo['docUserTagging'] = this.wrapUserDefinedTag(childNode.innerHTML)
+                    break;
+                case "topic":
+                    docInfo['docTopicL1'] = childNode.innerHTML
+                    docInfo['docTopicL1Order'] = (childNode.attributes['topic_order'])? childNode.attributes['topic_order'].value : "0"
+                    break;
+                case "doc_id":
+                    docInfo['docId'] = childNode.innerHTML
+                    break;
+                case "doc_time_created":
+                    docInfo['doc_time_created'] = childNode.innerHTML
+                    break;
+                case "xml_format_name":
+                    docInfo['xmlFormatName'] = childNode.innerHTML
+                    break;
+                case "db":
+                    docInfo['db'] = childNode.innerHTML
+                    break;
+                // case "extra_metadata":
+                //     docInfo['extraMetadata'] = childNode.innerHTML
+                //     break;
+                default:
+                    console.log('Unknow information: ' + childNode.nodeName)
+                    break;
+            }
+        })
+        docInfo['docFilename'] = filename
+        docInfo['timeInfo'] = timeInfo
+        docInfo['placeInfo'] = placeInfo 
+        const {db, corpus} = docInfo
+        delete docInfo['db']
+        return { db, corpus , docInfo }
+    }
 }
